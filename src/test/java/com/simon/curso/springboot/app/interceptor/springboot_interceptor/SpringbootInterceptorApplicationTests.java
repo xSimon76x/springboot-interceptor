483ee8b0074655d6ab733e2b69d8436b6093bfb0
Simon Iván Bustamante Venegas <73963276+xSimon76x@users.noreply.github.com>
2025-07-20 17:43:33 -0400

    init

---
+++ src/test/java/com/simon/curso/springboot/app/interceptor/springboot_interceptor/SpringbootInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simon.curso.springboot.app.interceptor.springboot_interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
